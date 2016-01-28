9afab9b16af02a115f4586eea35c77ccccc3eb72
Modrzew <dev@modriv.net>
2016-01-28 21:47:41 +0100

    Simple chat service in Rust finished!
    Oh wait, I wanted to write a game server. Damn.

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::sync::mpsc;
 use std::thread;
 
@@ -5,44 +6,88 @@ use rustc_serialize::json;
 use ws;
 use uuid::Uuid;
 
+struct Message {
+    client_id: String,
+    content: String,
+    ws: Option<ws::Sender>,
+}
+
 struct Server {
-    receiver: mpsc::Receiver<String>,
+    receiver: mpsc::Receiver<Message>,
+    clients: HashMap<String, ws::Sender>
+}
+
+impl Server {
+    fn new(receiver: mpsc::Receiver<Message>) -> Server {
+        Server {
+            receiver: receiver,
+            clients: HashMap::new()
+        }
+    }
+
+    fn run(&mut self) {
+        println!("Initialized server");
+        while let Ok(msg) = self.receiver.recv() {
+            // Add client on new connection
+            if let Some(ws) = msg.ws {
+                self.clients.insert(msg.client_id, ws);
+                continue;
+            }
+            let content = msg.content.trim();
+            // Skip empty messages
+            if content.len() == 0 {
+                continue;
+            }
+            // Notify all other clients
+            for (id, client) in &self.clients {
+                if id.to_string() != msg.client_id {
+                    client.send(content).unwrap();
+                }
+            }
+            println!("{}: {}", msg.client_id, msg.content);
+        }
+    }
 }
 
 struct SingleClientHandler {
     ws: ws::Sender,
     id: String,
-    sender: mpsc::Sender<String>,
+    sender: mpsc::Sender<Message>,
 }
 
 impl ws::Handler for SingleClientHandler {
     fn on_open(&mut self, _: ws::Handshake) -> ws::Result<()> {
         self.id = Uuid::new_v4().to_simple_string();
+        self.sender.send(Message {
+            client_id: self.id.to_string(),
+            content: "".to_string(),
+            ws: Some(self.ws.clone()),
+        }).unwrap();
         Ok(())
     }
+
     fn on_message(&mut self, msg: ws::Message) -> ws::Result<()> {
-        self.sender.send(msg.as_text().unwrap().to_string());
-        println!("Something sent");
+        self.sender.send(Message {
+            client_id: self.id.to_string(),
+            content: msg.as_text().unwrap().to_string(),
+            ws: None
+        }).unwrap();
         Ok(())
     }
 }
 
 pub fn start() {
-    let (sender, receiver): (mpsc::Sender<String>, mpsc::Receiver<String>) = mpsc::channel();
-    // let server = Server {
-    //     receiver: receiver,
-    // };
+    let (message_sender, message_receiver): (mpsc::Sender<Message>, mpsc::Receiver<Message>) = mpsc::channel();
+    let mut server = Server::new(message_receiver);
+
     thread::spawn(move || {
-        println!("Initialized listener");
-        while let Ok(msg) = receiver.recv() {
-            println!("Got {}", msg);
-        }
+        server.run()
     });
 
     ws::listen("127.0.0.1:3012", |out| {
         SingleClientHandler{
             ws: out,
-            sender: sender.clone(),
+            sender: message_sender.clone(),
             id: "".to_string()
         }
     }).unwrap();
